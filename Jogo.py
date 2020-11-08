4cdd6aae51c33c4d36e9ed827768f1f413331859
pedro <pedroa3@al.insper.edu.br>
2020-11-08 12:15:56 -0300

    é isso

--- Jogo.py
+++ Jogo.py
@@ -1,6 +1,22 @@
-#Testeluisa
+#Importando as bibliotecas
+import pygame
+import random
 
-#testepepe
+pygame.init()
 
-#TesteThiThi
+largura = 480
+altura = 600
+janela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Space Frogersor')
 
+controle= True
+
+while controle:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:  
+            controle = False
+
+    janela.fill((255,255,255))
+    pygame.display.update()
+
+pygame.quit()
