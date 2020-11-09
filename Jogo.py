886bfa9be30f536642227e640786d35f77fc83ab
thiagosk <thiagosk1@al.insper.edu.br>
2020-11-09 11:57:43 -0300

    Dia1

--- Jogo.py
+++ Jogo.py
@@ -1,22 +1,56 @@
 #Importando as bibliotecas
 import pygame
 import random
+import math
 
 pygame.init()
 
-largura = 480
-altura = 600
+branco = (255,255,255)
+preto = (0,0,0)
+largura = 800
+altura = 650
+
 janela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Space Frogersor')
+pygame.display.set_caption('Space Frogerson')
 
-controle= True
+nave_largura = 50
+nave_altura = 38
+nave_img = pygame.image.load('referencia/referencia/assets/img/playerShip1_orange.png').convert_alpha()
+nave_img = pygame.transform.scale(nave_img, (nave_largura, nave_altura))
 
-while controle:
+class nave(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 325
+        self.vx = 0
+
+    def update(self):
+        self.rect.x += self.vx
+
+
+todas_sprites = pygame.sprite.Group()
+jogador = nave(nave_img)
+todas_sprites.add(jogador)
+
+controle= 0
+while controle != 1:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:  
-            controle = False
+            controle = 1
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_a:
+                jogador.vx -= 8
+            elif evento.key == pygame.K_d:
+                jogador.vx += 8
 
-    janela.fill((255,255,255))
+    todas_sprites.update()
+    janela.fill(preto)
+    todas_sprites.draw(janela)
+
     pygame.display.update()
 
 pygame.quit()
