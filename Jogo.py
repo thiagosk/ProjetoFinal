ac1fd579ee2bcd19e3b2e004e67b11db92a93b2c
thiagosk <thiagosk1@al.insper.edu.br>
2020-11-20 18:20:52 -0300

    (LIVESHARE: todos) Capricho

--- Jogo.py
+++ Jogo.py
@@ -13,7 +13,7 @@ altura = 750
 screen = pygame.display.set_mode((largura, altura))
 
 #nome da tela
-pygame.display.set_caption('Navinhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+pygame.display.set_caption('navinhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 
 #icone da tela
 nave_img = pygame.image.load('assets/img/ufo.png').convert_alpha()
@@ -23,6 +23,7 @@ pygame.display.set_icon(nave_img)
 #cria imagens a tamanhos desejaveis
 nave_img = pygame.image.load('assets/img/ufo.png').convert_alpha()
 nave_img = pygame.transform.scale(nave_img, (30, 30))
+nave_img2 = pygame.transform.scale(nave_img, (120, 120))
 
 bala_img = pygame.image.load('assets/img/rec.png').convert_alpha()
 bala_img = pygame.transform.scale(bala_img, (20, 20))
@@ -32,35 +33,50 @@ a_meteoro = 30
 meteoro_img = pygame.image.load('assets/img/asteroid.png').convert_alpha()
 meteoro_img = pygame.transform.scale(meteoro_img, (l_meteoro, a_meteoro))
 
+thiagao_img = pygame.image.load('assets/img/thiagao.png').convert_alpha()
+thiagao_img = pygame.transform.scale(thiagao_img, (200,300))
+
+luisao_img = pygame.image.load('assets/img/luisao.png').convert_alpha()
+luisao_img = pygame.transform.scale(luisao_img, (200, 300))
+
+pedrao_img = pygame.image.load('assets/img/pedro.png').convert_alpha()
+pedrao_img = pygame.transform.scale(pedrao_img, (200, 300))
+
+
 score_font = pygame.font.Font('assets/font/kindergarten.ttf', 30)
 score_font2 = pygame.font.Font('assets/font/kindergarten.ttf', 50)
 score_font3 = pygame.font.Font('assets/font/kindergarten.ttf', 80)
 score_font4 = pygame.font.Font('assets/font/kindergarten.ttf', 60)
 score_font6 = pygame.font.Font('assets/font/kindergarten.ttf', 100)
+score_font7 = pygame.font.Font('assets/font/kindergarten.ttf', 21)
+score_font8 = pygame.font.Font('assets/font/kindergarten.ttf', 25)
 
-fundo = pygame.image.load('assets/img/fundo.png').convert()
+fundo = pygame.image.load('assets/img/starfield.png').convert()
 fundo = pygame.transform.scale(fundo, (largura, altura))
 
-fundo2 = pygame.image.load('assets/img/fundo.png').convert()
+fundo2 = pygame.image.load('assets/img/starfield.png').convert()
 fundo2 = pygame.transform.scale(fundo2, (largura, altura))
 
-fundo4 = pygame.image.load('assets/img/fundo.png').convert()
+fundo4 = pygame.image.load('assets/img/starfield.png').convert()
 fundo4 = pygame.transform.scale(fundo4, (largura, altura))
 
 #cria animacoes
 explosion_anim = []
 for i in range(5):
     filename = 'assets/img/explosion{}.png'.format(i)
-    img = pygame.image.load(filename).convert()
+    img = pygame.image.load(filename).convert_alpha()
     img = pygame.transform.scale(img, (32, 32))
     explosion_anim.append(img)
 
 #cria sons
-pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
-pygame.mixer.music.set_volume(0.01)
-botao_sound = pygame.mixer.Sound('assets/snd/expl3.wav')
-destroy_sound = pygame.mixer.Sound('assets/snd/expl6.wav')
-pew_sound = pygame.mixer.Sound('assets/snd/pew.wav')
+pygame.mixer.music.load('assets/snd/som4.flac')
+botao_sound = pygame.mixer.Sound('assets/snd/som1.wav')
+destroy_sound = pygame.mixer.Sound('assets/snd/som3.wav')
+pew_sound = pygame.mixer.Sound('assets/snd/som2.wav')
+pygame.mixer.music.set_volume(0.05)
+botao_sound.set_volume(0.1)
+destroy_sound.set_volume(0.1)
+pew_sound.set_volume(0.1)
 
 
 class Jogador(pygame.sprite.Sprite):
@@ -259,9 +275,15 @@ while estado == "inicio":
     FPS = 30
 
     #cria botao
-    button1 = pygame.Rect(316, 300, 120, 50)
-    button2 = pygame.Rect(285, 430, 200, 50)
-    button4 = pygame.Rect(280, 585, 220, 40)
+    button1 = pygame.Rect(320, 300, 120, 60)
+    button2 = pygame.Rect(277, 430, 210, 50)
+    button4 = pygame.Rect(280, 580, 220, 50)
+
+    #tempo inicial
+    tempo = 35
+
+    #balas iniciais
+    balass = 10
 
     clock.tick(FPS)
     for evento in pygame.event.get():
@@ -275,12 +297,8 @@ while estado == "inicio":
                 if button1.collidepoint(mouse_posicao):
                     botao_sound.play()
 
-                    #tempo inicial e inicio da contagem       
-                    tempo = 30
+                    #inicio da contagem       
                     start_ticks=pygame.time.get_ticks()
-
-                    #balas iniciais
-                    balass = 10
 
                     #score inicial
                     score = 0
@@ -313,9 +331,14 @@ while estado == "inicio":
                                 screen.blit(texto, text_rect)
 
                                 #escreve a pontuacao 
-                                texto = score_font2.render("Pontuacao:{0}".format(score), True, (100, 255, 100))
+                                texto = score_font2.render("Pontuacao:", True, (200, 255, 100))
                                 text_rect = texto.get_rect()
-                                text_rect = (160,  300)
+                                text_rect = (270,  300)
+                                screen.blit(texto, text_rect)
+
+                                texto = score_font2.render("{0}".format(score), True, (200, 255, 100))
+                                text_rect = texto.get_rect()
+                                text_rect = (350,  365)
                                 screen.blit(texto, text_rect)
 
                                 #substitui o score max
@@ -381,9 +404,14 @@ while estado == "inicio":
                                     screen.blit(texto, text_rect)
 
                                     #escreve a pontuacao
-                                    texto = score_font2.render("Pontuacao:{0}".format(score), True, (100, 255, 100))
+                                    texto = score_font2.render("Pontuacao:", True, (200, 255, 100))
                                     text_rect = texto.get_rect()
-                                    text_rect = (160,  300)
+                                    text_rect = (270,  300)
+                                    screen.blit(texto, text_rect)
+
+                                    texto = score_font2.render("{0}".format(score), True, (200, 255, 100))
+                                    text_rect = texto.get_rect()
+                                    text_rect = (350,  365)
                                     screen.blit(texto, text_rect)
 
                                     #substitui o score maximo
@@ -485,9 +513,75 @@ while estado == "inicio":
                         text_rect.midtop = (400, 70)
                         screen.blit(texto, text_rect)
 
+                        #escreve girar para a direita
+                        texto = score_font.render("girar para a direita", True,  (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (480, 300)
+                        screen.blit(texto, text_rect)
+
+                        #escreve girar para a esquerda
+                        texto = score_font.render("girar para a esquerda", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (500, 400)
+                        screen.blit(texto, text_rect)
+
+                        #escreve atirar
+                        texto = score_font.render("atirar", True,  (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (390, 500)
+                        screen.blit(texto, text_rect)
+
+                        #escreve velocidade 2x
+                        texto = score_font.render("velocidade 2x ", True,  (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (450,600)
+                        screen.blit(texto, text_rect)
+
+                        #escreve (somente possivel quando voce estiver sem
+                        texto = score_font7.render("(somente possivel quando estiver sem", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (530, 640) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve balas)
+                        texto = score_font7.render("balas)", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (720, 660) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve A
+                        texto = score_font3.render("A", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (200, 380) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve D
+                        texto = score_font3.render("D", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (200, 280) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve SPACE
+                        texto = score_font3.render("SPACE", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (200, 480) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve R
+                        texto = score_font3.render("R", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (200,580) 
+                        screen.blit(texto, text_rect)
+
+                        #escreve (somente possivel quando voce estiver sem balas)
+                        texto = score_font7.render("Acerte o maximo de meteoros que conseguir em {0} segundos e com {1} balas!".format(tempo,balass), True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (375, 200)
+                        screen.blit(texto, text_rect)
+
                         #imagem do botao de volta
                         button3 = pygame.Rect(35, 50, 90,27)
-                        pygame.draw.rect(screen, [40,40,10], button3)
+                        pygame.draw.rect(screen, [40,100,10], button3)
 
                         #escreve voltar
                         texto = score_font.render("Voltar", True, (0, 255, 255))
@@ -533,6 +627,42 @@ while estado == "inicio":
                         text_rect = texto.get_rect()
                         text_rect.midtop = (80, 50)
                         screen.blit(texto, text_rect)
+
+                        #escreve Pedro
+                        texto = score_font.render("Pedro", True, (255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (140, 220)
+                        screen.blit(texto, text_rect)
+
+                        #escreve Andrade
+                        texto = score_font.render("Andrade", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (140,250)
+                        screen.blit(texto, text_rect)
+
+                        #escreve Luis
+                        texto = score_font.render("Luis", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (380, 220)
+                        screen.blit(texto, text_rect)
+
+                        #escreve Bordignon
+                        texto = score_font.render("Bordignon", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (380, 250)
+                        screen.blit(texto, text_rect)
+
+                        #escreve Thiago
+                        texto = score_font.render("Thiago", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (600, 220)
+                        screen.blit(texto, text_rect)
+
+                        #escreve Shiguero
+                        texto = score_font.render("Shiguero", True,(255,100,100))
+                        text_rect = texto.get_rect()
+                        text_rect.midtop = (600, 250)
+                        screen.blit(texto, text_rect)
  
                         pygame.display.update()
 
@@ -553,29 +683,37 @@ while estado == "inicio":
                         screen.fill((0, 0, 0))
                         screen.blit(fundo2, (0, 0))
 
+                        screen.blit(luisao_img, (280, 300))
+
+                        screen.blit(thiagao_img, (510, 300))
+
+                        screen.blit(pedrao_img, (50, 300))
+
         #cor da tela
         screen.fill((0, 0, 0))
         screen.blit(fundo4, (0, 0))
 
         #desenha retangulo do botao
-        pygame.draw.rect(screen, [0,255,255], button1)
-        pygame.draw.rect(screen, [0,255,255], button2)
-        pygame.draw.rect(screen, [0,255,255], button4)
+        pygame.draw.rect(screen, [255,0,255], button1)
+        pygame.draw.rect(screen, [255,0,255], button2)
+        pygame.draw.rect(screen, [255,0,255], button4)
 
         #escreve navinhaaaaaaaaa
-        texto = score_font3.render("Navinhaaaaaaaaaaaaaa", True, (255,0,0))
+        texto = score_font3.render("navinhaaaaaaaaaaaaaa", True, (0,255,255))
         text_rect = texto.get_rect()
-        text_rect = (50, 100)
+        text_rect = (130, 88)
         screen.blit(texto, text_rect)
 
+        screen.blit(nave_img2, (10, 70))
+
         #escreve a pontuacao max
-        texto = score_font.render("Maior pontuacao:{0}".format(max_p), True, (100, 255, 100))
+        texto = score_font.render("Maior pontuacao: {0}".format(max_p), True, (50, 255, 50))
         text_rect = texto.get_rect()
-        text_rect = (50,  220)
+        text_rect = (30,  240)
         screen.blit(texto, text_rect)
 
         #mostra o PLAY do botao
-        texto = score_font4.render("Play", True, (0, 0, 0))
+        texto = score_font4.render("Play", True, (0, 0,0))
         text_rect = texto.get_rect()
         text_rect = (320, 300)
         screen.blit(texto, text_rect)
